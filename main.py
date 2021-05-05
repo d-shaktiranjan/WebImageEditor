f73c6504acd2c8b2f5978888c9d33c8af1207ecb
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-05-05 16:22:02 +0530

    convert img to grayscale & send

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, send_file
+import cv2 as cv
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    img = cv.imread('testPic.jpg', 0)
+    cv.imwrite('temp/edit.jpg', img)
+    return send_file("temp/edit.jpg", as_attachment=True)
 
 
 if __name__ == "__main__":
