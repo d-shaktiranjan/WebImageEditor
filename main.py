f9dbc253b6520d030190f726a90ef83bcf2e346f
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-05-05 20:33:01 +0530

    delete dir after send

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from flask import Flask, send_file, render_template
 import cv2 as cv
+from os import remove, mkdir
+from shutil import rmtree
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     img = cv.imread('testPic.jpg', 0)
-    cv.imwrite('temp/edit.jpg', img)
+    try:
+        mkdir("temp")
+    except:
+        pass
+    cv.imwrite('temp/edit3.jpg', img)
     return render_template("index.html")
 
 
 @app.route("/download")
 def download():
-    return send_file("temp/edit.jpg", as_attachment=True)
+    try:
+        return send_file("temp/edit3.jpg", as_attachment=True)
+    except:
+        return "Unable to Download"
+    finally:
+        rmtree("temp", ignore_errors=True)
 
 
 if __name__ == "__main__":
